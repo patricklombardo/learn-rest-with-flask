677c46e14f02ef4ad2cfc5fb9498c4a6e7282421
Patrick Lombardo <plombardo@opentable.com>
2023-06-30 11:32:39 -0700

    add exercise

---
+++ exercise/app.py
@@ -0,0 +1,28 @@
+# Imports
+
+import json
+
+from flask import Flask
+
+# 1. Initialize the Application
+
+#   a. create the Flask app
+
+#   b. load the "database" into a python dictionary from the JSON file
+
+
+# 2. Create REST endpoints for the following methods
+
+# /items - Retrieve all items in the database
+
+# /items/<id> - Retrieve the item with a given ID
+# This should fail if an item with the given it doesn't exist
+
+# /items/<id> - Update the item with a given ID
+# This should fail if an item with the given id does not exist
+
+# /items/<id> - Create a new item with a given ID
+# This should fail if an item with the given ID already exists
+
+# /items/<id> - Delete an item with the given id
+# This should fail if an item with the given ID doesn't exist
---
+++ exercise/readme.md
@@ -0,0 +1,7 @@
+# REST Exercises
+
+You are creating a REST library to manage inventory at a stationery shop.
+
+In `app.py` in this folder, follow the instructions to create REST endpoints with the correct methods for the given description.
+
+Your "database" will need to be loaded from `assets/data.json` and may live in a python dictionary or the data structure of your choosing.
